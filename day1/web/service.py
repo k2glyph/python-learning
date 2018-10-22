43e9816ccf637ccb9e431a01ed0051a63553ac31
Dinesh katwal <dinesh.katwal@logicabeans.com>
2018-10-22 17:22:48 +0545

    Created web api using flask

---
+++ day1/web/service.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+     return "Hello world"
+
+@app.route("/user/<username>")
+def show_user(username):
+    return "User: %s" %username
