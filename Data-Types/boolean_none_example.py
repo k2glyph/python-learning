b78d4ee559e7e1fa95ed306b946507e77fb0349d
Dinesh katwal <dinesh.katwal@logicabeans.com>
2018-10-23 20:42:38 +0545

    Created simple example of boolean and NOne

---
+++ Data-Types/boolean_none_example.py
@@ -0,0 +1,10 @@
+python_course = True
+java_course = False
+# Type casting of boolean to other
+int(python_course)==1
+int(java_course)==0
+str(python_course)=="True"
+
+# Using None Type
+aliens_found = None
+print(aliens_found)
