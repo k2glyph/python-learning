5c513b4a107b11569d215ea2513c0ce66044ad26
Dinesh katwal <dinesh.katwal@logicabeans.com>
2019-03-08 20:45:43 +0545

    Worked on gcd algorithms

---
+++ Programming-Foundations-algorithms/lessons-1-gcd.py
@@ -0,0 +1,18 @@
+'''
+This is based on simple algorithms based on Euclidean algorithms where
+    96=60.1+36
+    60=36.1+24
+    36=24.1+12 
+    24=12.2+0 => Here is the zero so. previous reminder is 12 hence the gcd of 96 and 60 is 12
+'''
+
+def gcd(a, b):
+    while(b !=0):
+        t=a
+        a=b
+        b=t%b
+    return a
+# Try out the function with few examples
+print(gcd(96,60)) # should be 12
+print(gcd(10,13)) # should be 1
+print(gcd(1701,3768)) # should be 3
